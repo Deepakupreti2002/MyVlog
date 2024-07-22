ceebc734b375c5e0138ee887535effb65053c0d7
= <deepakupreti875@gmail.com>
2024-07-22 14:16:48 +0530

    Final update

---
+++ Megavlog/src/components/index.js
@@ -0,0 +1,41 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout'
+import Home from "./pages/Home";
+import AllPosts from "./pages/AllPosts";
+import AddPost from "./pages/AddPost";
+import EditPost from "./pages/EditPost";
+import Post from "./pages/Post";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Home,
+    AllPosts,
+    AddPost,
+    EditPost,
+    Post
+
+}
